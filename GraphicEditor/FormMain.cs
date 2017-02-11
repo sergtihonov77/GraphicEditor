c6e6f40bcff8fa4eaad49a8c75cf3f7c2d496f46
sergtihonov77 <sergtihonov77@gmail.com>
2017-02-11 04:24:51 +0300

    Добавил проект установщик
    Немного почистил

--- GraphicEditor/FormMain.cs
+++ GraphicEditor/FormMain.cs
@@ -1,15 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.IO;
 using System.Drawing.Imaging;
-using System.Threading;
 
 namespace GraphicEditor
 {
